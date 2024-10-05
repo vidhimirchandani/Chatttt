96767717b5f73e9a106cb22698a6992518c53ba6
vidhimirchandani <141728735+vidhimirchandani@users.noreply.github.com>
2024-10-05 21:01:59 +0530

    Add files via upload

---
+++ utils/messages.js
@@ -0,0 +1,13 @@
+const generateMessage = (username,text) =>
+{
+    return {
+        username,
+        text,
+        createdAt: new Date().getTime()
+    }
+}
+
+
+module.exports = {
+    generateMessage
+}
---
+++ utils/users.js
@@ -0,0 +1,62 @@
+const users=[];
+
+const addUser = ({id, username,room})=>
+{
+    // clean the data
+    username = username.trim().toLowerCase();
+    room = room.trim().toLowerCase();
+    // validate the data
+    if(!username || !room)
+    {
+        return {
+            error:"Username and room are required!"
+        }
+    }
+    // check for existing user
+    const existingUser = users.find((user) =>{
+        return user.room=== room && user.username === username
+    });
+    // validate username
+    if(existingUser)
+    {
+        return {
+            error:"Username is is in use!"
+        }
+    }
+    // store user
+    const user = {id, username,room};
+    users.push(user);
+    return {user};
+};
+
+const removeUser = (id)=>
+{
+    const index = users.findIndex((user)=>
+    {
+        return user.id === id
+    });
+    if(index!== -1)
+    {
+        return users.splice(index,1)[0];
+    }
+};
+
+const getUser = (id)=>{
+    const user = users.find(obj=>
+        {
+            return obj.id===id;
+        });
+        return user;
+}
+
+const getusersInRoom = (room)=>
+{
+    return users.filter((user)=> user.room === room);
+}
+
+module.exports={
+    addUser,
+    removeUser,
+    getUser,
+    getusersInRoom
+};
